0f73bec81eee1e99065b09ba5a6daedcf3752174
YAKSHMEHTA <yakshvardhansinghmehta@gmail.com>
2026-05-02 19:46:43 +0530

    testing and fixed some issues

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-import { app,BrowserWindow } from "electron";
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
-const createWindow = ()=>{
+let overlayWin;
+
+const createWindow = () => {
     const win = new BrowserWindow({
-        width:1200,
-        height:800,
-        autoHideMenuBar:true
-    })
+        width: 1200,
+        height: 800,
+        autoHideMenuBar: true,
+        icon: path.join(__dirname, 'icon.png'),
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: false
+        }
+    });
     win.loadURL('https://leetcode.com');
-}
+    win.webContents.setWindowOpenHandler(({ url }) => {
+    
+});
+};
 
-app.whenReady().then(createWindow);
+
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
